2e06b3619e6c283345c43f470f42d3368e6c095d
unknown <once6474@naver.com>
2026-05-07 18:55:54 +0900

    NavBar 및 Hero update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react'
 import Navbar from './components/layout/Navbar'
 import Hero from './components/sections/Hero'
+import About from './components/sections/About'
 
 const App = () => {
   const [theme, setTheme] = useState('light')
@@ -17,6 +18,7 @@ const App = () => {
     <div>
       <Navbar theme={theme} toggleTheme={toggleTheme} />
       <Hero />
+      <About />
     </div>
   )
 }
